d24acddbc16177dcf5ae0b603609165fd2bffb10
otarampinelli <otaviorampinellielias@gmail.com>
2020-02-20 16:18:41 -0300

    doing backend with nodejs

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const server = express();
 // configurar o servidor para apresentar arquivos estáticos
 server.use(express.static('public'));
 
+// habilitar body do form
+server.use(express.urlencoded({ extended: true }));
+
 // configurando a template engine
 const nunjucks = require("nunjucks");
 nunjucks.configure("./", {
@@ -37,7 +40,22 @@ server.get("/", function(req, res) {
     return res.render("index.html", { donors });
 });
 
+server.post("/", function(req, res) {
+    // pegar dados do formulario
+    const name = req.body.name;
+    const email = req.body.email;
+    const blood = req.body.blood;
+
+    // coloca valores dentro do array
+    donors.push({ 
+        name: name,
+        blood: blood,
+     })
+
+     return res.redirect("/");
+});
+
 // ligar o servidor e permitir o acesso na porta 3000
 server.listen(3000, function() {
-    console.log("iniciei o servidor");
+    console.log("started server");
 });
